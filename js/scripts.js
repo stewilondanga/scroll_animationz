0a635c08209c2548914b90b1ace10a4423d17658
Stewart <stilondanga@gmail.com>
2024-02-10 17:03:58 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,30 +6,31 @@ var navigate = (function() {
     $('.' + dataName).toggle();
   });
 })();
-/*
+
 "use strict";
-(function () {
-	window.onload = () => {
-		const obj = document.querySelector("#gallery");
-		const time = 10000;
-		function animStart() {
-			if (obj.classList.contains("active") == false) {
-				obj.classList.add("active");
-				setTimeout(() => {
-					animEnd();
-				}, time);
-			}
-		}
-		function animEnd() {
-			obj.classList.remove("active");
-			obj.offsetWidth;
-		}
-		document.addEventListener("scroll", function () {
-			// scroll or scrollend
-			animStart();
-		});
-		window.addEventListener("resize", animStart);
-		animStart();
-	};
+(function() {
+  window.onload = () => {
+    const obj = document.querySelector("#gallery");
+    const time = 10000;
+
+    function animStart() {
+      if (obj.classList.contains("active") == false) {
+        obj.classList.add("active");
+        setTimeout(() => {
+          animEnd();
+        }, time);
+      }
+    }
+
+    function animEnd() {
+      obj.classList.remove("active");
+      obj.offsetWidth;
+    }
+    document.addEventListener("scroll", function() {
+      // scroll or scrollend
+      animStart();
+    });
+    window.addEventListener("resize", animStart);
+    animStart();
+  };
 })();
-*/
